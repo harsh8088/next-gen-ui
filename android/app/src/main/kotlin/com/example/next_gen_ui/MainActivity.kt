fb71e49f6b4eccc215b946468a8481860f55c6df
Harsh Bangari <harshrock2010@gmail.com>
2024-10-10 16:31:06 +0530

    initial commit

---
+++ android/app/src/main/kotlin/com/example/next_gen_ui/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.next_gen_ui
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/title_screen.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+
+import '../assets.dart';
+
+class TitleScreen extends StatelessWidget {
+  const TitleScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      body: Center(
+        child: Stack(
+          children: [
+            /// Bg-Base
+            Image.asset(AssetPaths.titleBgBase),
+
+            /// Bg-Receive
+            Image.asset(AssetPaths.titleBgReceive),
+
+            /// Mg-Base
+            Image.asset(AssetPaths.titleMgBase),
+
+            /// Mg-Receive
+            Image.asset(AssetPaths.titleMgReceive),
+
+            /// Mg-Emit
+            Image.asset(AssetPaths.titleMgEmit),
+
+            /// Fg-Rocks
+            Image.asset(AssetPaths.titleFgBase),
+
+            /// Fg-Receive
+            Image.asset(AssetPaths.titleFgReceive),
+
+            /// Fg-Emit
+            Image.asset(AssetPaths.titleFgEmit),
+          ],
+        ),
+      ),
+    );
+  }
+}
